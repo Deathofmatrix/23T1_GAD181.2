9012c415018e74330d65cd9ddd962ee7d64e9c1a
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-03-16 13:44:43 +1100

    upgrade one working on

---
+++ Assets/Scripts/UpgradeOne.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeOne : MonoBehaviour
+{
+
+    // need access to sheep prefab
+    // need variable for time last sheep
+    // need variable for time of next sheep
+
+    // need bool to check if script is enabled or not
+
+    [SerializeField] private GameObject sheepPrefab;
+    private float spawnTime = 1f;
+    private float lastSpawn = 0f;
+    private bool onField = false;
+
+    // instantiate the sheepPrefab every 1 second
+    // script needs to check if 1 second has passed
+    // if the time of 1 second hasn't passed - do nothing
+    // if 1 second has passed then instantiate
+
+    private void Update()
+    {
+        if(spawnTime < lastSpawn)
+        {
+            return;
+        }
+
+        else
+        {
+            spawnTime = Time.time + lastSpawn;
+            Instantiate(sheepPrefab);
+            sheepPrefab.transform.position = new Vector3(39, 0, -2);
+        }
+    }
+
+}
