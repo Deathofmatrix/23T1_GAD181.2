6e7316d2bea9e4c4e251342565888c0aefa07236
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-03-18 12:58:32 +1100

    added code comments related to the Bitwise Operation

--- Assets/Scripts/SheepDestroy.cs
+++ Assets/Scripts/SheepDestroy.cs
@@ -11,6 +11,10 @@ namespace SheepGame.Chonnor
 
         public void OnTriggerEnter(Collider other)
         {
+            // the 1 << is a Bitwise operation, which is used to check if the specific layer exists in the array of Layer Masks in the inspector
+            // it functions by literaaly shifting the ""0" and"1" in the bit to count up from 0
+            // I have tried this script without this incredibly conveluted method - It does not work
+
             if (SheepLayer == (SheepLayer | (1 << other.gameObject.layer)))
             {
                 Destroy(other.gameObject);
