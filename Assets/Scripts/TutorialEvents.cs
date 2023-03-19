5873e82972b978a0b16932c69557afab7edab689
IAmMrLuck <114543523+IAmMrLuck@users.noreply.github.com>
2023-03-19 16:00:47 +1100

    the first 4 tutorial pop ups work!

--- Assets/Scripts/TutorialEvents.cs
+++ Assets/Scripts/TutorialEvents.cs
@@ -9,24 +9,27 @@ public class TutorialEvents : MonoBehaviour
 
     private float waitTime = 3f;
     private float dragWaitTime = 6f;
+    private bool alreadyShownOne = false;
+    private bool alreadyShownTwo = false;
+    private bool alreadyShownThree = false;
+    // private bool alreadyShownFour = false;
 
-    public Button upgradeOne;
+    // public Button upgradeOne;
 
-    [SerializeField] private Image tryButton, watchSheep, moneyUp, tryShop, enoughmoney, dragNDrop;
-   // [SerializeField] private Image tryClicking;
+    [SerializeField] private Image tryButton, watchSheep, moneyUp, tryShop, enoughMoney, dragNDrop;
+    // [SerializeField] private Image tryClicking;
 
     private void Start()
     {
-        StartCoroutine(ScreenTime()); // not necessary but i wanted to try out a timed message
-        // timed messages might be useful later on when we include news reports
+        StartCoroutine(ScreenTime()); 
 
         watchSheep.enabled = false;
         moneyUp.enabled = false;
         tryShop.enabled = false;
-        enoughmoney.enabled = false;
-        dragNDrop.enabled = false;
-
-       // tryClicking.enabled = false;
+        enoughMoney.enabled = false;
+        
+        // dragNDrop.enabled = false;
+        // tryClicking.enabled = false;
 
     }
 
@@ -41,37 +44,42 @@ public class TutorialEvents : MonoBehaviour
         {
             return;
         }
-            
         else
         {
             Destroy(watchSheep);
         }
     }
 
-    /*public void TryClicking()
-    {
-        tryClicking.enabled = true;
-    }*/
+
 
     public void MoneyUp()
     {
         moneyUp.enabled = true;
+        alreadyShownOne = true;
+        Debug.Log("Now True");
+        StartCoroutine(ScreenTimeTwo());
     }
 
     public void TryShop()
     {
         tryShop.enabled = true;
+        alreadyShownTwo = true;
+        StartCoroutine(ScrrenTimeThree());
     }
 
     public void EnoughMoney()
     {
-        enoughmoney.enabled = true;
+        enoughMoney.enabled = true;
+        alreadyShownThree = true;
+        StartCoroutine(ScreenTimeFour());
     }
 
-    public void DragAndDrop()
-    {
-        dragNDrop.enabled = true;
-    }
+    //public void DragAndDrop()
+    //{
+      //  dragNDrop.enabled = true;
+        //alreadyShownFour = true;
+        //StartCoroutine(ScreenTimeFive());
+    //}
 
 
 
@@ -80,48 +88,63 @@ public class TutorialEvents : MonoBehaviour
         yield return new WaitForSecondsRealtime(waitTime);
         tryButton.enabled = false;
 
-        yield return new WaitForSecondsRealtime(waitTime);
-        watchSheep.enabled = false;
-
-
-        yield return new WaitForSecondsRealtime(waitTime);
-        moneyUp.enabled = false;
-
-        yield return new WaitForSecondsRealtime(waitTime);
-        tryShop.enabled=false;
-
-        yield return new WaitForSecondsRealtime(waitTime);
-        enoughmoney.enabled = false;
-
+        //yield return new WaitForSecondsRealtime(waitTime);
+        //watchSheep.enabled = false;
         //yield return new WaitForSecondsRealtime(waitTime);
         //tryClicking.enabled = false;
-
-        yield return new WaitForSeconds(dragWaitTime);
-        dragNDrop.enabled=false;
+        //yield return new WaitForSeconds(dragWaitTime);
+        //dragNDrop.enabled = false;
     }
 
+    IEnumerator ScreenTimeTwo()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        moneyUp.enabled = false;
+    }
 
+    IEnumerator ScrrenTimeThree()
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        tryShop.enabled = false;
+    }
+
+    IEnumerator ScreenTimeFour()
+
+    {
+        yield return new WaitForSecondsRealtime(waitTime);
+        enoughMoney.enabled = false;
+    }
+
+    IEnumerator ScreenTimeFive()
+    {
+        yield return new WaitForSeconds(dragWaitTime);
+        dragNDrop.enabled = false;
+
+    }
     private void Update()
     {
 
-         if (MoneyManager.currentMoney >= 1)
+        if (MoneyManager.currentMoney >= 1 && alreadyShownOne == false)
         {
             MoneyUp();
+          //  Debug.Log("Money Up");
         }
-        
-        if(MoneyManager.currentMoney >= 10)
+        //Debug.Log("Return Test");
+
+        if (MoneyManager.currentMoney >= 10 && alreadyShownTwo == false)
         {
             TryShop();
+          //  Debug.Log("Money 10");
         }
 
-         if(MoneyManager.currentMoney >= 50)
+        if (MoneyManager.currentMoney >= 50 && alreadyShownThree == false)
         {
             EnoughMoney();
         }
 
-         if (MoneyManager.currentMoney >= 50 && Input.GetButtonDown("Upgrade One"))
-        {
-            DragAndDrop();
-        }
+        //if (MoneyManager.currentMoney >= 500 && Input.GetButtonDown("Upgrade One") && alreadyShownFour == false)
+        //{
+        //    DragAndDrop();
+        //}
     }
 }
